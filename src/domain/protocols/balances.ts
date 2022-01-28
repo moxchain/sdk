ad52c72ecfbac5a258ef525a2c2b8ed2f1e43e7e
Leonardo Bonetti <leonardo.bonetti@outlook.com>
2022-01-28 18:10:48 -0300

    feat: add balances modules

--- src/domain/protocols/balances.ts
+++ src/domain/protocols/balances.ts
@@ -1,4 +1,6 @@
+import { UnsignedTransaction } from '@substrate/txwrapper-core'
+
 export interface BalancesHandler {
-  get: (accountId: string) => Promise<number>
-  transfer: (amount: number, to: string) => Promise<number>
+  get: (accountId: string) => Promise<string>
+  transfer: (amount: number, to: string, era: number) => Promise<UnsignedTransaction>
 }
---
+++ src/modules/balances/index.ts
@@ -0,0 +1,33 @@
+import { BalancesHandler } from '@/domain/protocols/balances'
+import { RpcHandler } from '@/domain/protocols/rpc'
+import { UnsignedTransaction, TypeRegistry } from '@substrate/txwrapper-core'
+import { TransactionHandler } from '@/domain/protocols/transaction'
+import { balancesTransfer } from '@/methods/balances/transfer'
+
+export class Balances implements BalancesHandler {
+  constructor (
+    private readonly rpc: RpcHandler,
+    private readonly transaction: TransactionHandler,
+    private readonly registry: TypeRegistry,
+    private readonly metadataRpc: string
+  ) {}
+
+  async get (accountId: string): Promise<string> {
+    return await this.rpc.get(`storage/getMoxBalance/${accountId}`)
+  }
+
+  async transfer (amount: number, to: string, era): Promise<UnsignedTransaction> {
+    const transactionInfo = await this.transaction.constructInfo(era)
+    const unsigned = balancesTransfer({
+      dest: to,
+      value: amount
+    },
+    transactionInfo,
+    {
+      metadataRpc: this.metadataRpc,
+      registry: this.registry
+    }
+    )
+    return unsigned
+  }
+}
