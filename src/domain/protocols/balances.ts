ed2cdf87c3eaca2b425fe5026cc8cb8122a59e79
Leonardo Bonetti <leonardo.bonetti@outlook.com>
2022-02-18 11:50:18 -0300

    fix: change balance transfer amount param to string

--- src/domain/protocols/balances.ts
+++ src/domain/protocols/balances.ts
@@ -2,5 +2,5 @@ import { UnsignedTransaction } from '@substrate/txwrapper-core'
 
 export interface BalancesHandler {
   get: (accountId: string) => Promise<string>
-  transfer: (amount: number, to: string, era: number) => Promise<UnsignedTransaction>
+  transfer: (amount: string, to: string, era: number) => Promise<UnsignedTransaction>
 }
